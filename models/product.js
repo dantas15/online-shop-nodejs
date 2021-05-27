da9aed217ae06582a5308717bade796f3a36df3a
Gustavo Dantas Galote <gusgalote@outlook.com>
2021-05-27 10:16:19 -0300

    Storing and Fetching Data in files via the Model

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
 class Product {
   constructor(title) {
@@ -6,11 +7,35 @@ class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (error, fileContent) => {
+      let products = [];
+      if (!error) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (error) => {
+        console.log(error);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
   }
 }
 
