6be01307fcf3b0d0c9c983b49f6f0bb259878f4b
gusgalote <gusgalote@outlook.com>
2021-06-03 13:57:03 -0300

    Passing Data with POST Requests

--- controllers/shop.js
+++ controllers/shop.js
@@ -40,6 +40,12 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCard = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+  res.redirect("/cart");
+};
+
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", {
     pageTitle: "Checkout",
--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,18 @@ const router = require("express").Router();
 const shopController = require("../controllers/shop");
 
 router.get("/", shopController.getIndex);
+
 router.get("/products", shopController.getProducts);
+
 router.get("/product/:productId", shopController.getProduct);
+
 router.get("/cart", shopController.getCart);
+router.post("/cart", shopController.postCard);
+
 router.get("/orders", shopController.getOrders);
+
 router.get("/checkout", shopController.getCheckout);
+
 router.get("/product-detail", shopController.getProductDetail);
 
 module.exports = router;
