058c93b27f4a856a627745aa341bfa2b6941bc14
gusgalote <gusgalote@outlook.com>
2021-06-03 13:36:17 -0300

    Loading and rendering Product Detail

--- controllers/shop.js
+++ controllers/shop.js
@@ -24,8 +24,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  console.log(prodId);
-  res.redirect("/");
+  Product.findById(prodId, (product) => {
+    res.render("shop/product-detail", {
+      pageTitle: product.title,
+      path: "/products",
+      product: product,
+    });
+  });
 };
 
 exports.getCart = (req, res, next) => {
