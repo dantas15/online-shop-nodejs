be96b2fd40e622b41b2ac2fc0a1f32cc319c8ebe
gusgalote <gusgalote@outlook.com>
2021-06-11 16:39:34 -0300

    Connecting the app to a SQL database

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const path = require("path");
 
 const express = require("express");
 
+const errorController = require("./controllers/error");
+const db = require("./util/database");
+
 const app = express();
 
 app.set("view engine", "ejs");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const errorController = require("./controllers/error");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
