072d5d786ef81487982cddee3cdbf16dd86afe9b
Gustavo Dantas Galote <gusgalote@outlook.com>
2021-07-05 03:25:58 -0300

    Sequelize: Creating, Fetching and Adding a new Product to the Cart

--- app.js
+++ app.js
@@ -58,7 +58,10 @@ sequelize
     return user;
   })
   .then((user) => {
-    console.log(user);
+    // console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000);
   })
   .catch((err) => {
