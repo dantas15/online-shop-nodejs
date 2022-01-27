4615f2fe5f4761cf00df2332d73cc705d75bdd92
Gustavo Dantas Galote <gusgalote@outlook.com>
2022-01-26 22:18:16 -0300

    working on cart items and orders

--- models/User.js
+++ models/User.js
@@ -2,9 +2,10 @@ const mongodb = require("mongodb");
 const getDB = require("../util/database").getDB;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
   }
 
   save() {
@@ -15,6 +16,19 @@ class User {
       .insertOne(this)
       .then((result) => console.log(result))
       .catch((err) => console.log(err));
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDB();
+
+    db.collection("users").updateOne(
+      { _id: mongodb.ObjectId(this._id) },
+      { $set: { cart: updatedCart } }
+    );
   }
 
   static findById(userId) {
